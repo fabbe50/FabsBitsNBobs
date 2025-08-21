03d5d65207a187c41a9d75a11e9b35c2d173f363
William <fabbe50@users.noreply.github.com>
2025-08-21 18:07:46 +0200

    Config Option Builder + Requires Restart
    Config option builder for boolean and integer values.
    These option types now also has support for requiring restart.

--- common/src/main/java/com/fabbe50/fabsbnb/config/BooleanOption.java
+++ common/src/main/java/com/fabbe50/fabsbnb/config/BooleanOption.java
@@ -2,10 +2,13 @@ package com.fabbe50.fabsbnb.config;
 
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
+import me.shedaniel.clothconfig2.impl.builders.BooleanToggleBuilder;
 
 import java.util.Properties;
 
 public class BooleanOption extends AbstractConfigOption<Boolean, BooleanListEntry> {
+    private boolean requiresRestart = false;
+
     public BooleanOption(String name, Boolean defaultValue) {
         super(name, defaultValue);
     }
@@ -16,15 +19,39 @@ public class BooleanOption extends AbstractConfigOption<Boolean, BooleanListEntr
 
     @Override
     public BooleanListEntry buildClothEntry(ConfigEntryBuilder builder) {
-        return builder.startBooleanToggle(getTranslation(), getValue())
+        BooleanToggleBuilder booleanBuilder = builder.startBooleanToggle(getTranslation(), getValue())
                 .setDefaultValue(getDefaultValue())
                 .setTooltip(getTooltipTranslation())
-                .setSaveConsumer(this::setValue)
-                .build();
+                .setSaveConsumer(this::setValue);
+        booleanBuilder.requireRestart(requiresRestart);
+        return booleanBuilder.build();
     }
 
     @Override
     public void readData(Properties properties) {
         setValue(Boolean.parseBoolean((String) properties.computeIfAbsent(getKey(), o -> String.valueOf(getDefaultValue()))));
+    }
+
+    public static class Builder {
+        String name;
+        boolean defaultValue;
+        boolean requiresRestart;
+
+        public Builder(String name, boolean defaultValue) {
+            this.name = name;
+            this.defaultValue = defaultValue;
+            this.requiresRestart = true;
+        }
+
+        public Builder requiresRestart() {
+            this.requiresRestart = true;
+            return this;
+        }
+
+        public BooleanOption build() {
+            BooleanOption option = new BooleanOption(name, defaultValue, defaultValue);
+            option.requiresRestart = requiresRestart;
+            return option;
+        }
     }
 }
--- common/src/main/java/com/fabbe50/fabsbnb/config/IntegerOption.java
+++ common/src/main/java/com/fabbe50/fabsbnb/config/IntegerOption.java
@@ -3,11 +3,14 @@ package com.fabbe50.fabsbnb.config;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.gui.entries.IntegerListEntry;
 import me.shedaniel.clothconfig2.gui.entries.IntegerSliderEntry;
+import me.shedaniel.clothconfig2.impl.builders.IntFieldBuilder;
 import net.minecraft.network.chat.Component;
 
 import java.util.Properties;
 
 public class IntegerOption extends AbstractRangedConfigOption<Integer, IntegerListEntry> {
+    private boolean requiresRestart = false;
+
     public IntegerOption(String name, Integer defaultValue) {
         this(name, defaultValue, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
@@ -22,17 +25,59 @@ public class IntegerOption extends AbstractRangedConfigOption<Integer, IntegerLi
 
     @Override
     public IntegerListEntry buildClothEntry(ConfigEntryBuilder builder) {
-        return builder.startIntField(getTranslation(), getValue())
+        IntFieldBuilder field = builder.startIntField(getTranslation(), getValue())
                 .setDefaultValue(getDefaultValue())
                 .setMin(min())
                 .setMax(max())
                 .setTooltip(getTooltipTranslation())
-                .setSaveConsumer(this::setValue)
-                .build();
+                .setSaveConsumer(this::setValue);
+        field.requireRestart(requiresRestart);
+        return field.build();
     }
 
     @Override
     public void readData(Properties properties) {
         setValue(Integer.parseInt((String) properties.computeIfAbsent(getKey(), o -> String.valueOf(getDefaultValue()))));
+    }
+
+    public static class Builder {
+        String name;
+        int defaultValue;
+        int minValue;
+        int maxValue;
+        boolean requiresRestart;
+
+        public Builder(String name, int defaultValue) {
+            this(name, defaultValue, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+
+        public Builder(String name, int defaultValue, int min, int max) {
+            this.name = name;
+            this.defaultValue = defaultValue;
+            this.minValue = min;
+            this.maxValue = max;
+            this.requiresRestart = true;
+        }
+
+        public IntegerOption.Builder requiresRestart() {
+            this.requiresRestart = true;
+            return this;
+        }
+
+        public IntegerOption.Builder min(int min) {
+            this.minValue = min;
+            return this;
+        }
+
+        public IntegerOption.Builder max(int max) {
+            this.maxValue = max;
+            return this;
+        }
+
+        public IntegerOption build() {
+            IntegerOption option = new IntegerOption(name, defaultValue, minValue, maxValue);
+            option.requiresRestart = requiresRestart;
+            return option;
+        }
     }
 }
