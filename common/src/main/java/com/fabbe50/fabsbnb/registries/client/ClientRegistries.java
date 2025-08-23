16f272361677912dc699c9c0643bfa06db350537
William <fabbe50@gmail.com>
2025-08-23 02:59:21 +0200

    Adjustments

--- common/src/main/java/com/fabbe50/fabsbnb/registries/client/ClientRegistries.java
+++ common/src/main/java/com/fabbe50/fabsbnb/registries/client/ClientRegistries.java
@@ -8,8 +8,10 @@ import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.registry.ReloadListenerRegistry;
 import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
+import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import dev.architectury.registry.menu.MenuRegistry;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.world.item.alchemy.PotionContents;
 
 public class ClientRegistries {
     public static void init() {
@@ -18,6 +20,16 @@ public class ClientRegistries {
         }
         ClientLifecycleEvent.CLIENT_SETUP.register(minecraft -> {
             BlockEntityRendererRegistry.register(ModRegistries.XP_HOLDER_BLOCK_ENTITY.get(), XPHolderRenderer::new);
+
+            ModRegistries.POTION_LIST.forEach(potionRegistrySupplier -> {
+                ColorHandlerRegistry.registerItemColors(
+                        (itemStack, layer) -> layer == 0 ? PotionContents.getColor(ModRegistries.getPotionReference(potionRegistrySupplier)) : -1,
+                        ModRegistries.OWN_POTION_ITEM.get(),
+                        ModRegistries.OWN_SPLASH_POTION_ITEM.get(),
+                        ModRegistries.OWN_LINGERING_POTION_ITEM.get(),
+                        ModRegistries.OWN_TIPPED_ARROW_ITEM.get()
+                );
+            });
         });
         ReloadListenerRegistry.register(PackType.SERVER_DATA, CustomFoodDataLoader.INSTANCE);
     }
